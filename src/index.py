42384c5a9489d142620e35983d5264fca5146b2b
EL023\EL23 <haemahmtea@gmail.com>
2026-01-06 13:01:18 +0900

    dd

--- src/index.py
+++ src/index.py
@@ -241,7 +241,6 @@ def main():
         #     submission_path=FOLDSEEK_FILE
         # )
 
-        
         # 4. 결과 저장
         # final_save_path = os.path.join(OUTPUT_DIR, "final_results.tsv")
         # final_df.to_csv(final_save_path, sep='\t', index=False)
